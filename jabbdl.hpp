b546c4ce2ca998c2aba60a49d582d4319e9caf57
Salazar99 <mr.nicoletti99@gmail.com>
2022-09-16 12:53:10 +0200

    Written functions for input parsing

--- jabbdl.hpp
+++ jabbdl.hpp
@@ -1,21 +1,65 @@
-//represent an entry in the unique table of vertices
-struct entry_table{
-    char *variable;
-    int high,low;
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <string>
+#include <optional>
+
+namespace jabbdl {
+
+struct variable { std::string name; };
+
+enum class expr_type {
+    Add,
+    Not,
+    Mul,
+    Var
 };
 
-//unique table to represent the vertices of all robdds
-struct entry_table *unique_table;
+struct expr;
+using expr_ptr = std::shared_ptr<expr>;
 
-//represent an expression 
-struct expr{
+struct expr_arg_2 { expr_ptr l; expr_ptr r; };
+struct expr_arg_1 { expr_ptr c; };
+struct expr_var   { variable name; };
 
+/// Represents an expression 
+struct expr {
+    expr_type type;
+    union {
+        expr_arg_2 args;
+        expr_arg_1 arg;
+        expr_var var;
+    };
+
+    explicit expr();
+    ~expr();
+
+    static expr_ptr make_add(expr_ptr& a, expr_ptr& b);
+    static expr_ptr make_mul(expr_ptr& a, expr_ptr& b);
+    static expr_ptr make_neg(expr_ptr& n);
+    static expr_ptr make_var(variable var);
+
+    static void print(const expr_ptr& expr);
 };
+
+expr_ptr ite(expr_ptr& a, expr_ptr& b, expr_ptr& c);
+
+expr create_expr(const std::string& f);
+
+struct vertex;
+using vertex_ptr = std::shared_ptr<vertex>;
 
 //structure to represent a vertex of the bdd
-struct vertex{
-    char* root;
-    struct vertex* lsubtree,*rsubtree;
+struct vertex {
+    variable root;
+    vertex_ptr lsubtree, rsubtree;
+
+    /// @brief Constructs a new vertex
+    /// @param root variable of the vertex
+    /// @param l left subtree
+    /// @param r right subtree
+    vertex(variable root, vertex_ptr l, vertex_ptr r);
 };
 
 //@input: root is the pointer to the root of the vertex to analyse, 
@@ -24,7 +68,7 @@ struct vertex{
 //@brief: The function takes a vertex in input and return:
 //          a new vertex if the one in input does not exists at the moment or
 //          a pointer to the vertex if it is already present
-struct vertex *old_or_new(char root, struct vertex *lst, struct vertex *rst);
+vertex_ptr old_or_new(variable root, vertex_ptr lst, vertex_ptr rst);
 
 
 //@input: "f" is the expression on wich the bdd must be built,
@@ -32,7 +76,7 @@ struct vertex *old_or_new(char root, struct vertex *lst, struct vertex *rst);
 //@output: pointer to the vertex of the robbd
 //@brief: Takes in input an expression and first variable and return the 
 //        pointer to the root of the robdd
-struct vertex *robdd_build(struct expr f, int i);
+vertex_ptr robdd_build(expr f, int i);
 
 
 //@input: unique_table is a pointer to the unique table of vertices;
@@ -40,4 +84,7 @@ struct vertex *robdd_build(struct expr f, int i);
 //        lst and rst are respectively the left and right subtree.         
 //@output: pointer to the vertex if found, otherwise nullptr;
 //@brief: Given a vertex, it checks if it is present inside the unique table and returns it
-struct vertex *lookup(struct entry_table *unique_table, char root, struct vertex *lst, struct vertex *rst);
+std::optional<vertex_ptr> lookup(const std::vector<vertex_ptr>& unique_table, variable root, vertex_ptr lst, vertex_ptr rst);
+
+
+} // namespace jabbd 
--- jabddl.cpp
+++ jabddl.cpp
@@ -1,17 +1,115 @@
 #include "jabbdl.hpp"
 
-struct vertex *old_or_new(char root, struct vertex *lst, struct vertex *rst){
-    vertex * v;
-    if((v = lookup(unique_table, root, lst, rst)) != nullptr){
-        return v;
+#include <sstream>
+#include <iostream>
+
+namespace jabbdl {
+
+//unique table to represent the vertices of all robdds
+static std::vector<vertex_ptr> unique_table;
+
+expr::expr() : type{expr_type::Var} { }
+expr::~expr() { }
+
+expr_ptr expr::make_add(expr_ptr& a, expr_ptr& b) {
+    auto res = std::make_shared<expr>();
+    res->type = expr_type::Add;
+    res->args.l = a;
+    res->args.r = b;
+    return res;
+}
+expr_ptr expr::make_mul(expr_ptr& a, expr_ptr& b) {
+    auto res = std::make_shared<expr>();
+    res->type = expr_type::Mul;
+    res->args.l = a;
+    res->args.r = b;
+    return res;
+}
+expr_ptr expr::make_neg(expr_ptr& n) {
+    auto res = std::make_shared<expr>();
+    res->type = expr_type::Not;
+    res->arg.c = n;
+    return res;
+}
+expr_ptr expr::make_var(variable var) {
+    auto res = std::make_shared<expr>();
+    res->var.name = var;
+    return res;
+};
+
+expr_ptr ite(expr_ptr& a, expr_ptr& b, expr_ptr& c) {
+
+    // If a then b
+    auto choise1 = expr::make_mul(a, b);
+    
+    // if not a then c
+    auto neg_a = expr::make_neg(a);
+    auto choise2 = expr::make_mul(neg_a, c);
+
+    // x1 is 1 or 0
+    return expr::make_add(choise1, choise2);
+}
+
+static void print_rec(std::stringstream& stream, const expr_ptr& expr) {
+    switch (expr->type) {
+        case expr_type::Add: {
+            stream << "(";
+            print_rec(stream, expr->args.l);
+            stream << " + ";
+            print_rec(stream, expr->args.r);
+            stream << ")";
+        } break;
+        case expr_type::Mul: {
+            stream << "(";
+            print_rec(stream, expr->args.l);
+            stream << " * ";
+            print_rec(stream, expr->args.r);
+            stream << ")";
+        } break;
+        case expr_type::Not: {
+            stream << "!";
+            print_rec(stream, expr->arg.c);
+        } break;
+        case expr_type::Var: {
+            stream << expr->var.name.name;
+        } break;
     }
+}
+
+void expr::print(const expr_ptr& expr) {
+    std::stringstream stream;
+    print_rec(stream, expr);
+    std::cout << stream.str() << std::endl;
+}
+
+vertex::vertex(variable root, vertex_ptr l, vertex_ptr r) 
+: root{root}, lsubtree{l}, rsubtree{r} { }
+
+vertex_ptr old_or_new(variable root, vertex_ptr lst, vertex_ptr rst){
+    
+    vertex_ptr v;
+    auto result = lookup(unique_table, root, lst, rst);
+    if (result.has_value())
+        return *result;
     else{
         /*create a new vertex*/
+        v = std::make_shared<vertex>(root, lst, rst);
         return v;
     }
 }
 
-struct vertex *robdd_build(struct expr f, int i){
+vertex_ptr robdd_build(struct expr f, int i) {
 
 
 }
+
+std::optional<vertex_ptr> lookup(const std::vector<vertex_ptr>& unique_table, variable root, vertex_ptr lst, vertex_ptr rst){
+    for (auto& vertex : unique_table) {
+        if (vertex->root.name == root.name && vertex->lsubtree == lst && vertex->rsubtree == rst)
+            return std::make_optional<vertex_ptr>(vertex);
+    }
+    return std::nullopt;
+}
+
+
+} // namespace jabbdl
