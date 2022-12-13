b11da060d6ea52c983ba2b56d88c080d18abac55
daniele nicoletti <magister@DESKTOP-R461MOV>
2022-12-13 20:13:26 +0100

    implemented cmake

---
+++ build/CMakeFiles/jabddl.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Daniele/Desktop/jabddl/src/expr.cpp" "CMakeFiles/jabddl.dir/src/expr.cpp.o" "gcc" "CMakeFiles/jabddl.dir/src/expr.cpp.o.d"
+  "/mnt/c/Users/Daniele/Desktop/jabddl/src/jabddl.cpp" "CMakeFiles/jabddl.dir/src/jabddl.cpp.o" "gcc" "CMakeFiles/jabddl.dir/src/jabddl.cpp.o.d"
+  "/mnt/c/Users/Daniele/Desktop/jabddl/src/main_test.cpp" "CMakeFiles/jabddl.dir/src/main_test.cpp.o" "gcc" "CMakeFiles/jabddl.dir/src/main_test.cpp.o.d"
+  "/mnt/c/Users/Daniele/Desktop/jabddl/src/vertex.cpp" "CMakeFiles/jabddl.dir/src/vertex.cpp.o" "gcc" "CMakeFiles/jabddl.dir/src/vertex.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/jabddl.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jabddl.dir/src/expr.cpp.o"
+  "CMakeFiles/jabddl.dir/src/expr.cpp.o.d"
+  "CMakeFiles/jabddl.dir/src/jabddl.cpp.o"
+  "CMakeFiles/jabddl.dir/src/jabddl.cpp.o.d"
+  "CMakeFiles/jabddl.dir/src/main_test.cpp.o"
+  "CMakeFiles/jabddl.dir/src/main_test.cpp.o.d"
+  "CMakeFiles/jabddl.dir/src/vertex.cpp.o"
+  "CMakeFiles/jabddl.dir/src/vertex.cpp.o.d"
+  "jabddl"
+  "jabddl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jabddl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
